298908ddbd91db0346eafcb8956540920ae4764d
Jobin T v <jobinvarghese18@gmail.com>
2022-10-21 10:59:52 +0530

    feat: Minimal footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Home from './pages/home';
 import React from 'react';
 import { Header } from './components/3-organisms/Header';
 import SignUp from './pages/sign-up';
-
-import './App.css';
-import 'antd/dist/antd.min.css';
 import SingInPage from './pages/sing-in';
 import { AppValueProvider } from './context/appContext';
 import { BooksValueProvider } from './context/bookContext';
+import { Footer } from './components/3-organisms/Footer';
+
+import './App.css';
+import 'antd/dist/antd.min.css';
 const App: React.FC = () => {
   return (
     <BrowserRouter>
@@ -21,6 +22,7 @@ const App: React.FC = () => {
             <Route path="/sign-in" element={<SingInPage />} />
             <Route path="/home" element={<Home />} />
           </Routes>
+          <Footer />
         </BooksValueProvider>
       </AppValueProvider>
     </BrowserRouter>
---
+++ src/components/3-organisms/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import tw from 'twin.macro';
+
+export const Footer: React.FC = () => {
+  return (
+    <Container>
+      <BrandNameContainer>
+        Book Store
+        <Text>A reader lives a thousand lives before he dies</Text>
+      </BrandNameContainer>
+      <CopyRighContainer>
+        Copyright © 2022 all rights reserved
+      </CopyRighContainer>
+    </Container>
+  );
+};
+
+const Container = tw.div`h-80 w-full flex flex-col justify-between bg-gray-800 text-white mt-10 pt-10 pl-10`;
+const BrandNameContainer = tw.div`text-4xl font-semibold`;
+const CopyRighContainer = tw.div`h-10 w-full text-center text-base`;
+const Text = tw.div`pt-3 text-base font-thin`;
